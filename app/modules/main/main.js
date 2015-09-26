bf94bdf218a9c5d6544e0ebb0fa1aff1e7ca8636
Hannes Widmoser <widmoser@gmail.com>
2015-09-26 21:35:54 +0200

    removed board directive

--- app/modules/main/main.js
+++ app/modules/main/main.js
@@ -5,6 +5,17 @@
 
         $scope.words = [ 'hello' ];
 
+        $scope.orderedWords = [];
+
+        $scope.onDragComplete = (index) => {
+            $scope.words.splice(index, 1);
+        };
+
+        $scope.onDropComplete = (data, event) => {
+            console.log(data, event);
+            $scope.orderedWords.push(data);
+        }
+
     }]);
 
 })();
